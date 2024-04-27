be4e98bc3dee2e9b2d1b2c8188c331f2f85bc0ea
Yoonjae Choi <dbswo9795@gmail.com>
2024-04-27 21:16:54 +0900

    feat: 버튼 컴포넌트 구

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,82 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { Flex, Spacing, Text } from '.';
+import {
+  ButtonColor,
+  ButtonSize,
+  buttonColorMap,
+  buttonSizeMap,
+  buttonWeakMap,
+} from '@/styles';
+
+interface ButtonProps {
+  color?: ButtonColor;
+  size?: ButtonSize;
+  weak?: boolean;
+  full?: boolean;
+  disabled?: boolean;
+}
+
+const BaseButton = styled.button<ButtonProps>(
+  {
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    borderRadius: '6px',
+  },
+  ({ color = 'primary', weak }) =>
+    weak ? buttonWeakMap[color] : buttonColorMap[color],
+  ({ size = 'small' }) => buttonSizeMap[size],
+  ({ full }) =>
+    full
+      ? css`
+          display: block;
+          width: 100%;
+          border-radius: 0;
+        `
+      : undefined,
+  ({ disabled }) =>
+    disabled
+      ? css`
+          opacity: 0.26;
+          cursor: initial;
+        `
+      : undefined
+);
+
+const ButtonGroup = ({
+  title,
+  children,
+}: {
+  title?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Flex direction="column">
+      {title && (
+        <>
+          <Text typography="t6" bold>
+            {title}
+          </Text>
+          <Spacing size={8} />
+        </>
+      )}
+      <Flex css={buttonGroupStyle}>{children}</Flex>
+    </Flex>
+  );
+};
+
+const buttonGroupStyle = css`
+  flex-wrap: wrap;
+  gap: 10px;
+
+  button {
+    flex: 1;
+  }
+`;
+
+export const Button = BaseButton as typeof BaseButton & {
+  Group: typeof ButtonGroup;
+};
+
+Button.Group = ButtonGroup;
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,4 @@
+export * from './Button';
 export * from './Flex';
 export * from './Input';
 export * from './ScrollToTop';
