fa40a6fcc56cee2f718a4b869041a0e7b463d210
Yoonjae Choi <dbswo9795@gmail.com>
2024-04-30 18:38:18 +0900

    feat: 이미지 리스트 데이터 추가

---
+++ src/constants/listData.ts
@@ -0,0 +1,34 @@
+export const IMAGE_LIST = [
+  {
+    path: '/images/1.webp',
+    name: '1',
+  },
+  {
+    path: '/images/2.webp',
+    name: '2',
+  },
+  {
+    path: '/images/3.webp',
+    name: '3',
+  },
+  {
+    path: '/images/4.webp',
+    name: '4',
+  },
+  {
+    path: '/images/5.webp',
+    name: '5',
+  },
+  {
+    path: '/images/6.webp',
+    name: '6',
+  },
+  {
+    path: '/images/7.webp',
+    name: '7',
+  },
+  {
+    path: '/images/8.webp',
+    name: '8',
+  },
+];
