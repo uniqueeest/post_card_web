b04b6c65d46e630ec49e4f722cc12c9cf05898c6
Yoonjae Choi <dbswo9795@gmail.com>
2024-05-01 12:14:03 +0900

    feat: 라우터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { ScrollToTop } from '@components/shared';
+import { MainPage, OrderPage } from './pages';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/order" element={<OrderPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
