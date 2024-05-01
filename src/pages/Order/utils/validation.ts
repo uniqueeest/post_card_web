f1a012da964395ded4f2507551194c80fec40543
Yoonjae Choi <dbswo9795@gmail.com>
2024-05-01 12:12:48 +0900

    feat: 유효성 함수 추가

---
+++ src/pages/Order/utils/validation.ts
@@ -0,0 +1,26 @@
+import { OrderValue } from '..';
+
+export function validateOrder(order: OrderValue): boolean {
+  if (
+    !order.name ||
+    !order.phoneNumber ||
+    !order.receiptMethod ||
+    order.selectedImage.length === 0
+  ) {
+    return false;
+  }
+
+  if (order.receiptMethod === '택배수령') {
+    const { address } = order;
+    if (
+      !address ||
+      !address.postcode ||
+      !address.address ||
+      !address.detailAddress
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
