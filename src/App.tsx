46893a899553bf305845f7b296b0d2e342fc31f7
Yoonjae Choi <dbswo9795@gmail.com>
2024-05-02 23:13:23 +0900

    fix: 어드민 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { ScrollToTop } from '@components/shared';
-import { MainPage, OrderPage } from './pages';
+import { MainPage, OrderPage, AdminPage } from './pages';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/order" element={<OrderPage />} />
-      </Routes>
+      <Suspense fallback={<div></div>}>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/order" element={<OrderPage />} />
+          <Route path="/admin_jyo" element={<AdminPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
