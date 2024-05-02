e9b0786143199d4f6418c6aee6697ac85e2fcdcf
Yoonjae Choi <dbswo9795@gmail.com>
2024-05-02 23:13:11 +0900

    fix: 리액트 쿼리 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.tsx';
 import { AlertContext } from './contexts/alertContext';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AlertContext>
-      <App />
-    </AlertContext>
+    <QueryClientProvider client={queryClient}>
+      <AlertContext>
+        <App />
+      </AlertContext>
+    </QueryClientProvider>
   </React.StrictMode>
 );
