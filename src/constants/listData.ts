3df7a7841fecc2d256b17190e70ef4a8a76244c3
Yoonjae Choi <dbswo9795@gmail.com>
2024-05-06 22:32:51 +0900

    fix: 솔드아웃 필드 추가

--- src/constants/listData.ts
+++ src/constants/listData.ts
@@ -2,34 +2,42 @@ export const IMAGE_LIST = [
   {
     path: '/images/1.webp',
     name: '1',
+    soldOut: false,
   },
   {
     path: '/images/2.webp',
     name: '2',
+    soldOut: false,
   },
   {
     path: '/images/3.webp',
     name: '3',
+    soldOut: false,
   },
   {
     path: '/images/4.webp',
     name: '4',
+    soldOut: false,
   },
   {
     path: '/images/5.webp',
     name: '5',
+    soldOut: false,
   },
   {
     path: '/images/6.webp',
     name: '6',
+    soldOut: true,
   },
   {
     path: '/images/7.webp',
     name: '7',
+    soldOut: false,
   },
   {
     path: '/images/8.webp',
     name: '8',
+    soldOut: false,
   },
 ];
 
