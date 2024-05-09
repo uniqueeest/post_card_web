55aa87bb3b537db0e255afd6aad450d5753bdc16
Yoonjae Choi <dbswo9795@gmail.com>
2024-05-10 00:57:00 +0900

    feat: 에러바운더리 추가

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+interface ErrorBoundaryProps {
+  fallback: React.ReactNode;
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    // You can also log the error to an error reporting service
+    logErrorToMyService(error, info.componentStack ?? '');
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+// Helper function to log errors to a service
+function logErrorToMyService(error: Error, componentStack: string) {
+  console.error('Logged Error:', error);
+  console.info('Component Stack:', componentStack);
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
+export * from './ErrorBoundary';
 export * from './PostCode';
 export * from './PostSwiper';
