c373c74aba7a54a16c8e08268ecf150f995d777f
Yoonjae Choi <dbswo9795@gmail.com>
2024-05-10 00:57:15 +0900

    fix: 에러바운더리 연결

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ScrollToTop } from '@components/shared';
 import { MainPage, OrderPage, AdminPage } from './pages';
 import { Suspense } from 'react';
+import { ErrorBoundary } from './components';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<div></div>}>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/order" element={<OrderPage />} />
-          <Route path="/admin_jyo" element={<AdminPage />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <ErrorBoundary fallback={<div>error</div>}>
+      <BrowserRouter>
+        <Suspense fallback={<div></div>}>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/order" element={<OrderPage />} />
+            <Route path="/admin_jyo" element={<AdminPage />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
